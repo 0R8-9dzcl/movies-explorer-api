ef8e51a7e1dede443e0580a000bcb5691850cb0f
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-03 19:26:03 +0300

    feat: add user schema

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require('validator/');
+const bcrypt = require('bcryptjs');
+const AuthError = require('../errors/AuthError');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    default: 'Александр',
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function loginUser(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        // ошибка 'Пользователь с такой почтой не найден' код 401
+        throw new AuthError('Неправильные почта или пароль');
+      }
+      // юзер найден
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            // ошибка 'Неправильные почта или пароль' код 401
+            throw new AuthError('Неправильные почта или пароль');
+          }
+          // аутентификация успешна
+          // отправка данных пользователя
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
