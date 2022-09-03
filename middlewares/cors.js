8b4f6930fb51f1ed6d9f9741c4723e78f9cb85ff
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-03 19:43:55 +0300

    feat:  add CORS middleware

---
+++ middlewares/cors.js
@@ -0,0 +1,27 @@
+const allowedCors = [
+  'https://movies.explorer.nomoredomains.sbs',
+  'http://movies.explorer.nomoredomains.sbs',
+  'http://localhost:3001',
+];
+// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.status(200).send();
+    return;
+  }
+  next();
+};
+
+module.exports = cors;
