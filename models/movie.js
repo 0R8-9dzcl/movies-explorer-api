07fd73faac35794fdcc0ca9294e7353e8bee3abc
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-03 19:26:33 +0300

    add movie schema

---
+++ models/movie.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const validator = require('validator/');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    required: true,
+    type: String,
+  },
+  director: {
+    required: true,
+    type: String,
+  },
+  duration: {
+    required: true,
+    type: Number,
+  },
+  year: {
+    required: true,
+    type: Number,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  image: {
+    required: true,
+    type: String,
+  },
+  trailerLink: {
+    required: true,
+    type: String,
+    validator: (link) => validator.isLink(link),
+  },
+  thumbnail: {
+    required: true,
+    type: String,
+    validator: (link) => validator.isLink(link),
+  },
+  owner: {
+    type: mongoose.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    required: true,
+    type: String,
+  },
+  nameEN: {
+    required: true,
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
