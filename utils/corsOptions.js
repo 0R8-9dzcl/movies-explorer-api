ed6e23177abcab6e3358177d7f1410e303734491
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-04 23:05:45 +0300

    fix: fix cors settings

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,15 +1,18 @@
 const whitelist = [
   'https://movies.explorer.nomoredomains.sbs',
   'http://movies.explorer.nomoredomains.sbs',
-  'http://localhost:3000',
+  'http://localhost:3001',
 ];
-
-module.exports = {
-  origin(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+const corsOptions = {
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,',
+  optionsSuccessStatus: 200,
+};
+module.exports = (req, callback) => {
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions.origin = true; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions.origin = false; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
 };
