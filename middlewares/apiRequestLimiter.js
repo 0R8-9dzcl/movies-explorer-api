3b2eba3976a70e63f7db7a71a499bf38ef1319d1
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-08 20:32:57 +0300

    fix: fix trow error

--- middlewares/apiRequestLimiter.js
+++ middlewares/apiRequestLimiter.js
@@ -1,8 +1,9 @@
 const rateLimit = require('express-rate-limit');
+const TooManyRequests = require('../errors/TooManyRequests');
 const errMess = require('../utils/errMess');
 
 module.exports = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 минута
   max: 6, // ограничить 6 запросами
-  message: errMess.tooMany.requests,
+  handler: (req, res, next) => next(new TooManyRequests(errMess.tooMany.requests)),
 });
