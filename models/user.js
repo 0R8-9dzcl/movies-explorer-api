30a7bb1d55e953cba1344a9164dedb2886a2147f
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-08 17:50:03 +0300

    feat: add conlict error text

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
+    unique: [true, errMess.conflict.email],
     validate: {
       validator: (email) => validator.isEmail(email),
       message: 'Неправильный формат почты',
